7085de2e04bd59349643088b14b71e49efa0e45e
teleport-palis <cpalis@teleport.earth>
2017-12-10 11:15:48 +0800

    update prod command

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-let url = '138.197.137.68:3000';
+let url = '127.0.0.1:3000';
 
 export const environment = {
   production: false,
